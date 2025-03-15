94ff298bd947a07e801737b3ad612cbeb7df2fe1
Brian <okellobrian18@gmail.com>
2025-03-15 11:27:59 +0300

    Page Layout without images

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ const ramens = [
     { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
     { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
  ];
+
